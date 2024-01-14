9c6e227a3d75147a8441c62906afcc3a32f137a6
cherry2cherry <1571973949@qq.com>
2024-01-14 21:21:50 +0800

    first commit

---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/lmz/test_ros/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH='/home/lmz/test_ros/devel/lib:/opt/ros/noetic/lib:/usr/local/cuda-11.7/lib64'
+export PATH='/opt/ros/noetic/bin:/usr/local/cuda-11.7/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin'
+export PKG_CONFIG_PATH="/home/lmz/test_ros/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
+export PWD='/home/lmz/test_ros/build'
+export ROSLISP_PACKAGE_DIRECTORIES='/home/lmz/test_ros/devel/share/common-lisp'
+export ROS_PACKAGE_PATH="/home/lmz/test_ros/src:$ROS_PACKAGE_PATH"
---
+++ build/helloworld/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/lmz/test_ros/src/helloworld
+# Build directory: /home/lmz/test_ros/build/helloworld
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
