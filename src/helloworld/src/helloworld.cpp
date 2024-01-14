26f22778996d03cd8f14c80d07fe79a8803e22a4
cherry2cherry <1571973949@qq.com>
2024-01-14 22:10:04 +0800

    111

--- src/helloworld/src/helloworld.cpp
+++ src/helloworld/src/helloworld.cpp
@@ -12,7 +12,7 @@ ros::Publisher chatter_pub = n.advertise<std_msgs::String>("chatter", 1000);
 ros::Rate loop_rate(10);
 int count = 0;
 while(ros::ok())
-{
+{    
 std_msgs::String msg;
 std::stringstream ss;
 ss << "hello world " << count;
@@ -20,7 +20,7 @@ msg.data = ss.str();
 ROS_INFO("%s", msg.data.c_str());
 chatter_pub.publish(msg);
 ros::spinOnce();
-loop_rate.sleep();
+loop_rate.sleep();   
 count++;
 }
 return 0;
